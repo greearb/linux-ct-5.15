6dc1091b1d3e07b2dae3f631b63d7fd7e50ecfb8
Ben Greear <greearb@candelatech.com>
2021-09-02 16:55:59 -0700

    mac80211/mt76: Allow disabling OFDMA (non upstream)
    This lets us disable OFDMA even if peer supports this.
    It did not actually solve the RF interference problem, but
    maybe useful for other things.
    
    Too much of a hack to go upstream most likely.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -2613,6 +2613,7 @@ struct cfg80211_auth_request {
  * @ASSOC_REQ_DISABLE_HE:  Disable HE
  * @ASSOC_REQ_DISABLE_TWT:  Disable TWT
  * @ASSOC_REQ_DISABLE_160:  Disable 160Mhz
+ * @ASSOC_REQ_DISABLE_OFDMA:  Disable OFDMA
  */
 enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_HT			= BIT(0),
@@ -2622,6 +2623,7 @@ enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_HE			= BIT(4),
 	ASSOC_REQ_DISABLE_TWT			= BIT(5),
 	ASSOC_REQ_DISABLE_160			= BIT(6),
+	ASSOC_REQ_DISABLE_OFDMA			= BIT(7),
 };
 
 /**
